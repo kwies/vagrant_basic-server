3351fe5165c99cfde3965aae35a13dff9fccda2f
kwies <kwiesgith@wolke7.net>
2015-05-29 08:09:35 +0200

    initial commit

---
+++ ubuntu/14.04/config/shell/base.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+if cd "`dirname \"$0\"`"; then
+    absdirpath=`pwd`
+    cd "$OLDPWD" || exit 1
+else
+    exit 1
+fi
+SCRIPTDIR=$absdirpath
+BASEDIR=$(dirname $SCRIPTDIR)
+SCRIPTNAME=$(basename $0 .sh)
+
+echo ""
+echo "===== $SCRIPTNAME ====="
+echo ""
+
+# execute system update (if needed)
+apt-get update -y
+#apt-get upgrade -y
+
+echo "add here your dependencies .."
+
